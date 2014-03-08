920cae22b3c590c569417800bb61796abe8564f9
ahl3h6 <ahl3h6@mail.umkc.edu>
2014-03-07 21:34:16 -0600

    Uploaded
    added log in button in home page

---
+++ KPIMetricsSystem/AspnetIdentitySample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KpiMetricsSystem.Startup))]
+namespace KpiMetricsSystem
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
